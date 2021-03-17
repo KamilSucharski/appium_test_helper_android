52b038ac3b6207ef3aefac38b10f0661c33c95b8
Kamil Sucharski <kamil.sucharski@apzumi.pl>
2021-03-17 19:22:10 +0100

    Granting content permissions to all installed packages

--- app/src/main/java/com/sengami/appium_test_helper_android/Consts.kt
+++ app/src/main/java/com/sengami/appium_test_helper_android/Consts.kt
@@ -4,18 +4,14 @@ object Consts {
     // Shared Preferences
     const val SHARED_PREFERENCES_NAME = "SHARED_PREFERENCES"
     const val KEY_TEST_MODE_ENABLED = "KEY_TEST_MODE_ENABLED"
-    const val KEY_PACKAGE_NAMES = "KEY_PACKAGE_NAMES"
 
     // Content Providers and Broadcast Receivers
     const val CONTENT_PROVIDER_URI = "content://com.sengami.appium_test_helper_android.provider/"
-    const val CONTENT_PROVIDER_TYPE = "dummy-type"
+    const val CONTENT_PROVIDER_TYPE = "appium-type"
     const val TOAST_MESSAGE_KEY = "message"
     const val VIBRATION_DURATION_KEY = "duration"
 
     // Extra
     const val EXTRA_LONG_VIBRATION_DURATION = "EXTRA_LONG_VIBRATION_DURATION"
     const val EXTRA_BOOLEAN_STOP_COMMAND = "EXTRA_BOOLEAN_STOP_COMMAND"
-
-    // Other
-    const val PACKAGE_NAME_SEPARATOR = "\n"
 }
--- app/src/main/java/com/sengami/appium_test_helper_android/MainActivity.kt
+++ app/src/main/java/com/sengami/appium_test_helper_android/MainActivity.kt
@@ -5,6 +5,7 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.sengami.appium_test_helper_android.databinding.ActivityMainBinding
@@ -20,59 +21,26 @@ class MainActivity : Activity() {
             R.layout.activity_main
         )
 
-        contentResolver
-            .getType(Uri.parse(Consts.CONTENT_PROVIDER_URI))
-            .takeIf { it == null }
-            ?.let {
-                Toast.makeText(
-                    this,
-                    R.string.error_content_provider,
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-
         val localStorage = LocalStorage.fromContext(this)
-
         binding.testModeSwitch.isChecked = localStorage.isTestModeEnabled()
         binding.testModeContainer.setOnClickListener {
             val newState = !localStorage.isTestModeEnabled()
             localStorage.setTestModeEnabled(newState)
             binding.testModeSwitch.isChecked = newState
         }
-
-        localStorage
-            .getPackageNames()
-            .joinToString(separator = Consts.PACKAGE_NAME_SEPARATOR)
-            .run(binding.packageNamesEditText::setText)
-
-        saveCurrentPackageNames(binding, localStorage)
-
-        binding.savePackageNamesButton.setOnClickListener {
-            saveCurrentPackageNames(binding, localStorage)
-        }
     }
 
-    private fun saveCurrentPackageNames(binding: ActivityMainBinding, localStorage: LocalStorage) {
-        binding
-            .packageNamesEditText
-            .text
-            .split(Consts.PACKAGE_NAME_SEPARATOR)
-            .map { it.trim().toLowerCase(Locale.getDefault()) }
-            .let { packageNames ->
-                localStorage.setPackageNames(packageNames)
-                packageNames.forEach { packageName ->
-                    grantUriPermission(
-                        packageName,
-                        Uri.parse(Consts.CONTENT_PROVIDER_URI),
-                        Intent.FLAG_GRANT_READ_URI_PERMISSION
-                    )
-                }
+    override fun onResume() {
+        super.onResume()
+        val uri = Uri.parse(Consts.CONTENT_PROVIDER_URI)
+        packageManager
+            .getInstalledPackages(0)
+            .forEach {
+                grantUriPermission(
+                    it.packageName,
+                    uri,
+                    Intent.FLAG_GRANT_READ_URI_PERMISSION
+                )
             }
-
-        Toast.makeText(
-            this,
-            R.string.message_package_names_set,
-            Toast.LENGTH_SHORT
-        ).show()
     }
 }
